d983b8e21d5fb7d9bfc338bb53d09a2a359bf789
BlazinZzetti <blazinzzetti@gmail.com>
2016-11-26 23:44:28 -0800

    Initial Source Commit
    Initial commit of the program's source code.  Contains a few files for
    features I plan to remove from this program and move to other projects.

---
+++ Assets/SharedAssets/Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMainMenu : MonoBehaviour
+{
+    public bool MenuMode = false;
+
+    public Canvas MenuCamera;
+
+    // Update is called once per frame
+	void Update ()
+    {
+        //Return to Main Menu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!MenuMode)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+            else
+            {
+                MenuCamera.enabled = !MenuCamera.enabled;
+            }
+        }
+    }
+}
